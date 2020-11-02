bf7ef5a1b57cae9b769ee7a55dba92d37de38a58
CharlesEricADAM <ce.adam.developer@gmail.com>
2020-11-02 10:09:07 +0100

    buildpack nodejs & ruby

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,9 @@ require 'rails/all'
 # config/application.rb
 config.assets.initialize_on_precompile = false
 
+heroku buildpacks:add --index 1 heroku/nodejs
+heroku buildpacks:add --index 2 heroku/ruby
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
